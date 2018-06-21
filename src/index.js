44613ddba576b16e6ce39896265cb01c98c427c8
Julio Montaño <jmz7v.com@gmail.com>
2018-06-21 13:19:58 -0500

    Create base router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Elysium = props => (
+  <Router>
+    <React.Fragment>
+      <Switch>
+        <Route path='/' component={App} />
+      </Switch>
+    </React.Fragment>
+  </Router>
+)
+
+ReactDOM.render(<Elysium />, document.getElementById('root'))
 registerServiceWorker();
