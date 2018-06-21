e3d3707e347ae3af32db1b9df06f852e8592e4ac
Julio Montaño <jmz7v.com@gmail.com>
2018-06-21 13:53:29 -0500

    Create router for private routes

---
+++ src/components/login/index.js
@@ -0,0 +1,14 @@
+// Libraries
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        Login screen
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -5,16 +5,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Components
 import App from 'components/app';
+import Login from 'components/login';
 
 // Other
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const Elysium = props => (
+const Private = (...props) => (
+  <Route
+    {...props}
+    render={p => 'Yo'}
+  />
+)
+
+// Main router, provides auth an non-auth routes
+const Elysium = () => (
   <Router>
     <React.Fragment>
       <Switch>
-        <Route path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route exact path='/' component={App} />
+        <Route path='/:route' component={Private} />
       </Switch>
     </React.Fragment>
   </Router>
