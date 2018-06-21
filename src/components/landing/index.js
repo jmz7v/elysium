aa487ccbebc98bacd4cd4c057b3a7860c256eca8
Julio Montaño <jmz7v.com@gmail.com>
2018-06-21 14:27:11 -0500

    Create simple dashboard page

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,24 +1,19 @@
 // Libraries
 import React, { Component } from 'react'
 
-// Components
-import logo from 'components/logo/logo.svg'
-
 // Other
 import './index.css'
 
 class Landing extends Component {
   render () {
     return (
-      <div className='Landing'>
-        <header className='Landing-header'>
-          <img src={logo} className='Landing-logo' alt='logo' />
-          <h1 className='Landing-title'>Welcome to React</h1>
-        </header>
-        <p className='Landing-intro'>
-          To get started, edit <code>src/Landing.js</code> and save to reload.
-        </p>
-      </div>
+      <section class='section'>
+        <div class='container'>
+          <h1 class='title'>Elysium</h1>
+          <h2 class='subtitle'>React app</h2>
+          <a class='button is-primary' href='/login'>Login</a>
+        </div>
+      </section>
     )
   }
 }
