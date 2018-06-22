ce648ee096dfa03433a4092929e3fc095e833563
Julio Montaño <jmz7v.com@gmail.com>
2018-06-22 13:27:39 -0500

    Create and render nav component

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -2,31 +2,14 @@
 import React, { Component } from 'react'
 
 // Components
+import Nav from 'components/nav'
 import Tabs from 'components/tabs'
 
 class Dashboard extends Component {
-  renderNav () {
-    return (
-      <nav className='navbar is-light' aria-label='main navigation'>
-        <div className='navbar-brand'>
-          <a className='navbar-item' href='https://bulma.io'>
-            <img src='https://bulma.io/images/bulma-logo.png' alt='Bulma: a modern CSS framework based on Flexbox' width='112' height='28' />
-          </a>
-
-          <a role='button' className='navbar-burger' aria-label='menu' aria-expanded='false'>
-            <span aria-hidden='true' />
-            <span aria-hidden='true' />
-            <span aria-hidden='true' />
-          </a>
-        </div>
-      </nav>
-    )
-  }
-
   render () {
     return (
       <React.Fragment>
-        {this.renderNav()}
+        <Nav />
         <Tabs />
       </React.Fragment>
     )
