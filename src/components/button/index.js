1bcd276c289856b687d8e26a83980096313a4acc
Julio Montaño <jmz7v.com@gmail.com>
2018-06-22 13:39:25 -0500

    Create base component for button

---
+++ src/components/button/index.js
@@ -0,0 +1,25 @@
+// Libraries
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Button extends Component {
+  constructor (props) {
+    super()
+    this.data = {
+    }
+    this.state = {...this.data}
+  }
+
+  render () {
+    return (
+    )
+  }
+}
+
+Button.propTypes = {
+}
+
+Button.defaultProps = {
+}
+
+export default Button
