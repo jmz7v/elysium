fd4dd2b6de1d5bbcadaefa9e4fcde859187e8696
Julio Montaño <jmz7v.com@gmail.com>
2018-06-22 13:56:32 -0500

    Add base behavior to button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,11 @@
 // Libraries
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+// Components
+
+// Other
 
 class Button extends Component {
   constructor (props) {
@@ -10,16 +15,42 @@ class Button extends Component {
     this.state = {...this.data}
   }
 
+  onClick () {
+    this.props.onClick()
+  }
+
   render () {
+    const { disabled, loading, className, kind, text } = this.props
     return (
+      <button
+        className={classNames('button', className, kind, {
+          'is-loading': loading
+        })}
+        onClick={() => { this.onClick() }}
+        disabled={disabled}
+      >
+        {text}
+      </button>
     )
   }
 }
 
 Button.propTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  text: PropTypes.string,
+  className: PropTypes.string,
+  kind: PropTypes.string
 }
 
 Button.defaultProps = {
+  onClick: () => {},
+  disabled: false,
+  loading: false,
+  text: 'button',
+  className: '',
+  kind: 'is-primary',
 }
 
 export default Button
