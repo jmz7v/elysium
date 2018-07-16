3e56abf670e72b38ab8c7ffbaa41e7973fb346d7
Julio Montaño <jmz7v.com@gmail.com>
2018-07-16 01:42:17 -0500

    Add prop behavior for type, disabled, valid, invalid

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,8 +1,15 @@
+// Libraries
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 class Input extends React.Component {
   state = {
-    value: ''
+    value: this.props.defaultValue,
+    type: this.props.type,
+    disabled: this.props.disabled,
+    valid: this.props.valid,
+    invalid: this.props.invalid
   }
 
   setValue = e => {
@@ -10,17 +17,39 @@ class Input extends React.Component {
   }
 
   render () {
-    const { value } = this.state
+    const { value, type, disabled, valid, invalid } = this.state
+    const className = classNames('input', {
+      'is-success': valid,
+      'is-danger': invalid
+    })
+
     return (
       <input
-        className="input"
-        type="text"
-        placeholder="Text input"
+        className={className}
+        type={type}
+        disabled={disabled}
+        placeholder='Text input'
         value={value}
         onChange={this.setValue}
       />
     )
   }
+}
+
+Input.propTypes = {
+  defaultValue: PropTypes.string,
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+  valid: PropTypes.bool,
+  invalid: PropTypes.bool
+}
+
+Input.defaultProps = {
+  defaultValue: '',
+  type: 'text',
+  disabled: false,
+  valid: false,
+  invalid: false
 }
 
 export default Input
