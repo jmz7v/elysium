2a48d1ede719b05ca244ccca28142775538ae906
Julio Montaño <jmz7v.com@gmail.com>
2018-07-20 12:40:33 -0500

    Create form object for login attempt

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -3,17 +3,47 @@ import React, { Component } from 'react'
 
 // Components
 import Title from 'components/title'
+import Form from 'components/form'
+
+const form = {
+  title: 'Ingresa',
+  fields: [
+    {
+      name: 'input',
+      label: 'email',
+      kind: 'text',
+    },
+    {
+      name: 'input',
+      label: 'password',
+      kind: 'password',
+    }
+  ],
+  primary: {
+    text: 'Ingresar',
+    action: () => { console.log('Ingresar') }
+  },
+  secondary: {
+    text: 'Olvidé mi contraseña',
+    action: () => { console.log('Recuperar') }
+  }
+}
 
 class Login extends Component {
   render () {
     return (
-      <section class='section'>
-        <div class='container'>
+      <section className='section'>
+        <div className='container'>
           <Title
             title='Inicia sesión'
             subtitle='O crea una cuenta'
           />
-          <a class='button is-primary' href='/dashboard'>Ingresar</a>
+          <Form
+            title={form.title}
+            primary={form.primary}
+            fields={form.fields}
+            secondary={form.secondary}
+          />
         </div>
       </section>
     )
