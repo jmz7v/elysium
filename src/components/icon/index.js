129cd33fb4a9f2897d4c0bbf7da7f9748b02465a
Julio Montaño <jmz7v.com@gmail.com>
2018-09-04 13:43:57 -0500

    Create Icon component

---
+++ src/components/icon/index.js
@@ -0,0 +1,18 @@
+// Libraries
+import React from 'react'
+import classNames from 'classnames'
+
+const sizes = {
+  baby: 'mdi-18px',
+  small: 'mdi-24px',
+  medium: 'mdi-36px',
+  large: 'mdi-48px'
+}
+
+// Button component
+const Icon = ({icon, spin, size}) => <i className={classNames('icon mdi', `mdi-${icon}`, {
+  'mdi-spin': spin,
+  [sizes[size]]: size
+})} />
+
+export default Icon
